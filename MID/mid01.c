19e050f64109fe2297c6b733a528d4794867170d
KutsunaSubaRya <index20010928@gmail.com>
2021-04-07 17:07:02 +0800

    add MID mid01.c

---
+++ MID/mid01.c
@@ -0,0 +1,314 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+typedef struct _sMixedNumber {
+	int32_t a;
+	int32_t b;
+	int32_t c;
+}sMixedNumber;
+
+int _gcd_(int32_t a, int32_t b)
+{
+	while(b!=0){
+		int tmp=a%b;
+		a=b;
+		b=tmp;
+	}
+	return a;
+}
+
+int mixed_set( sMixedNumber *pNumber , int32_t a, int32_t b, int32_t c)
+{
+	if(b!=c && c==0) return -1;
+	if(a!=0 && (b<0 || c<0)) return -1;
+	if(a==0 && (b<0 || c<0))
+	{
+		if(b<0 && c<0)
+		{
+			b=abs(b);
+			c=abs(c);
+		}
+		else if(c<0 && b>=0)
+		{
+			c*=-1;
+			b*=-1;
+		}
+	}
+	if(b==c && b==0) c=1;
+	pNumber->a=a;
+	pNumber->b=b;
+	pNumber->c=c;
+	return 0;
+}
+	
+// return -1 if invalid; otherwise , return 0.
+
+int mixed_print( const sMixedNumber number)
+{
+	if(number.b==0) printf("(%d,%d,%d)\n",number.a, number.b, 0);
+	else printf("(%d,%d,%d)\n",number.a, number.b, number.c);
+	return 0;
+}
+// in the form of (a,b,c)
+
+void mixed_add( sMixedNumber *pNumber , const sMixedNumber r1, const sMixedNumber r2)
+{
+	int r1b, r2b;
+	if(r1.a>=0)
+		r1b=r1.b+r1.a*r1.c;
+	else
+		r1b=r1.a*r1.c-r1.b;
+	if(r2.a>=0)
+		r2b=r2.b+r2.a*r2.c;
+	else
+		r2b=r2.a*r2.c-r2.b;
+	int32_t gcd=_gcd_(r1.c, r2.c), lcm=(r1.c*r2.c)/gcd;
+	pNumber->b=r1b*(lcm/r1.c)+r2b*(lcm/r2.c);
+	pNumber->c=lcm;
+	if(pNumber->b<0)
+	{
+		pNumber->b=abs(pNumber->b);
+		pNumber->a=(pNumber->b/pNumber->c)*-1;
+		pNumber->b%=pNumber->c;
+		gcd=_gcd_(pNumber->b,pNumber->c);
+		pNumber->b/=gcd;pNumber->c/=gcd;
+	}
+	else 
+	{
+		pNumber->a=pNumber->b/pNumber->c;
+		pNumber->b%=pNumber->c;
+		gcd=_gcd_(pNumber->b,pNumber->c);
+		pNumber->b/=gcd;pNumber->c/=gcd;
+	}
+}
+// pNumber = r1 + r2
+
+void mixed_sub( sMixedNumber *pNumber , const sMixedNumber r1, const sMixedNumber r2)
+{
+	int r1b, r2b;
+	if(r1.a>=0)
+		r1b=r1.b+r1.a*r1.c;
+	else
+		r1b=r1.a*r1.c-r1.b;
+	if(r2.a>=0)
+		r2b=r2.b+r2.a*r2.c;
+	else
+		r2b=r2.a*r2.c-r2.b;
+	int32_t gcd=_gcd_(r1.c, r2.c), lcm=(r1.c*r2.c)/gcd;
+	pNumber->b=r1b*(lcm/r1.c)-r2b*(lcm/r2.c);
+	pNumber->c=lcm;
+	if(pNumber->b<0)
+	{
+		pNumber->b=abs(pNumber->b);
+		pNumber->a=(pNumber->b/pNumber->c)*-1;
+		pNumber->b%=pNumber->c;
+		gcd=_gcd_(pNumber->b,pNumber->c);
+		pNumber->b/=gcd;pNumber->c/=gcd;
+	}
+	else
+	{
+		pNumber->a=pNumber->b/pNumber->c;
+		pNumber->b%=pNumber->c;
+		gcd=_gcd_(pNumber->b,pNumber->c);
+		pNumber->b/=gcd;pNumber->c/=gcd;
+	}
+}
+// pNumber = r1 - r2
+
+void mixed_mul( sMixedNumber *pNumber , const sMixedNumber r1, const sMixedNumber r2)
+{
+	int r1b, r2b;
+	if(r1.a>=0)
+		r1b=r1.b+r1.a*r1.c;
+	else
+		r1b=r1.a*r1.c-r1.b;
+	if(r2.a>=0)
+		r2b=r2.b+r2.a*r2.c;
+	else
+		r2b=r2.a*r2.c-r2.b;
+	pNumber->b=r1b*r2b;
+	pNumber->c=r1.c*r2.c;
+	//printf("b=%d c=%d\n",pNumber->b, pNumber->c);
+	int gcd=_gcd_(pNumber->b, pNumber->c);
+	pNumber->b/=gcd;
+	pNumber->c/=gcd;
+	if(pNumber->b<0)
+	{
+		pNumber->b=abs(pNumber->b);
+		pNumber->a=(pNumber->b/pNumber->c)*-1;
+		pNumber->b%=pNumber->c;
+		gcd=_gcd_(pNumber->b,pNumber->c);
+		pNumber->b/=gcd;pNumber->c/=gcd;
+	}
+	else
+	{
+		pNumber->a=pNumber->b/pNumber->c;
+		pNumber->b%=pNumber->c;
+		gcd=_gcd_(pNumber->b,pNumber->c);
+		pNumber->b/=gcd;pNumber->c/=gcd;
+	}
+}
+// pNumber = r1 * r2
+
+void mixed_div( sMixedNumber *pNumber , const sMixedNumber r1, const sMixedNumber r2)
+{
+	int r1b, r2b;
+	if(r1.a>=0)
+		r1b=r1.b+r1.a*r1.c;
+	else
+		r1b=r1.a*r1.c-r1.b;
+	if(r2.a>=0)
+		r2b=r2.b+r2.a*r2.c;
+	else
+		r2b=r2.a*r2.c-r2.b;
+	pNumber->b=r1b*r2.c;
+	pNumber->c=r1.c*r2b;
+	//printf("b=%d c=%d\n",pNumber->b, pNumber->c);
+	int gcd=_gcd_(pNumber->b, pNumber->c);
+	pNumber->b/=gcd;
+	pNumber->c/=gcd;
+	if(pNumber->b<0)
+	{
+		pNumber->b=abs(pNumber->b);
+		pNumber->a=(pNumber->b/pNumber->c)*-1;
+		pNumber->b%=pNumber->c;
+		gcd=_gcd_(pNumber->b,pNumber->c);
+		pNumber->b/=gcd;pNumber->c/=gcd;
+	}
+	else
+	{
+		pNumber->a=pNumber->b/pNumber->c;
+		pNumber->b%=pNumber->c;
+		gcd=_gcd_(pNumber->b,pNumber->c);
+		pNumber->b/=gcd;pNumber->c/=gcd;
+	}
+}
+// pNumber = r1 / r2}
+
+signed main()
+{
+    char str[10005];
+    sMixedNumber Stack[10005],tmp;
+    char Stack_AO[10005],c;
+    int ff=0;
+    printf("Q: ");
+    while((c=getchar()) && c!='\0' && c!=EOF && c!='\n')
+    {
+        if(c!=' ')
+        str[ff++]=c;
+    }
+    int len=strlen(str), Flag=0,AO_Flag=0,deter_A=0;
+    for(int i=0;i<len;i++)
+    {
+        int abc[5]={0},fl=0,deter=0;
+        if(deter_A==1 && str[i]=='(')
+        {
+            deter_A=0;
+            continue;
+        }
+        if(str[i]=='(')
+        {
+            //printf("Hi (\n");
+            for(int j=i+1;j<len;j++)
+            {
+                if(str[j]==')') 
+                {
+                    //printf("Hi )\n");
+                    break;
+                }
+                else if(str[j]==' ') continue;
+                else if(str[j]==',')
+                {
+                    if(deter) abc[fl]*=-1;
+                    fl++;
+                    deter=0;
+                }
+                else
+                {
+                    if(str[j]=='-') deter=1;
+                    if(abc[fl]==0)
+                    {
+                        abc[fl]=(int)str[j]-48;
+                    }
+                    else
+                    {
+                        abc[fl]*=10;
+                        abc[fl]+=(int)str[j]-48;
+                    }
+                }
+            }
+            mixed_set(&Stack[Flag],abc[0],abc[1],abc[2]);
+            Flag++;
+        }
+        if(str[i]=='*' || str[i]=='/')
+        {
+            //printf("Hi */\n");
+            char tmp_strchar=str[i];
+            Flag--;
+            tmp.a=Stack[Flag].a;
+            tmp.b=Stack[Flag].b;
+            tmp.c=Stack[Flag].c;
+            for(int j=i+1;j<len;j++)
+            {
+                if(str[j]==')') 
+                {
+                    break;
+                }
+                else if(str[j]==' ' || str[j]=='(') continue;
+                else if(str[j]==',')
+                {
+                    fl++;
+                }
+                else
+                {
+                    if(abc[fl]==0)
+                    {
+                        abc[fl]=(int)str[j]-48;
+                    }
+                    else 
+                    {
+                        abc[fl]*=10;
+                        abc[fl]+=(int)str[j]-48;
+                    }
+                }
+            }
+            sMixedNumber tmp_abc;
+            mixed_set(&tmp_abc, abc[0], abc[1], abc[2]);
+            if(tmp_strchar=='*') mixed_mul(&Stack[Flag],tmp,tmp_abc);
+            else mixed_div(&Stack[Flag],tmp,tmp_abc);
+            Flag++;
+            deter_A=1;
+            continue;
+        }
+        if(str[i]=='+' || str[i]=='-')
+        {
+            Stack_AO[AO_Flag++]=str[i];
+        }
+        //printf("Flag= %d, AO_Flag= %d\n",Flag,AO_Flag);
+        //mixed_print(Stack[Flag-1]);
+    }
+    //printf("Flag= %d, AO_Flag= %d\n",Flag,AO_Flag);
+    Flag--;
+    //printf("St= %c", Stack_AO[0]);
+    while(AO_Flag!=0)
+    {
+        AO_Flag--;
+        //printf("AO_Flag= %d\n",AO_Flag);
+        if(Stack_AO[AO_Flag]=='+')
+        {
+            mixed_add(&Stack[Flag-1], Stack[Flag-1], Stack[Flag]);
+            Flag--;
+        }
+        else if(Stack_AO[AO_Flag]=='-')
+        {
+            mixed_sub(&Stack[Flag-1], Stack[Flag-1], Stack[Flag]);
+            Flag--;
+        }
+    }
+    printf("A: ");
+    mixed_print(Stack[0]);
+    return 0;
+}
